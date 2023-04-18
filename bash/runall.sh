9cbd31c559a569dc6bc447ba9411cc2d434b35e5
carl <carl@123.org>
2023-04-18 10:42:59 +0800

    增加 -o 短选项以执行固定的模块

--- bash/runall.sh
+++ bash/runall.sh
@@ -25,15 +25,15 @@ function isInArray() {
   local module=$1
   shift
   local read_array=$@
-  for ignore in ${read_array[@]};do
-    if [[ ${ignore} == ${module} ]];then
+  for ignore in ${read_array[@]}; do
+    if [[ ${ignore} == ${module} ]]; then
       return 0
     fi
   done
   return 1
 }
 
-function doExecuteCmd() {
+function moduleExecuteCmd() {
   local module=$1
   shift
   log_normal "start to execute command [$*] in module [${module}]"
@@ -46,62 +46,93 @@ function doExecuteCmd() {
   return 0
 }
 
-
 error_module=()
 external_ignore=()
+only_execute_modules=()
 declare -i count=0
 
 function push_external_ignore_module() {
   local module_name=$1
+  shift
   module_name=${module_name%/*}
   isInArray ${module_name} ${external_ignore}
-  isExternalIgnore=$(echo $?)
-  if [[ ${isExternalIgnore} -eq 1 ]];then
+  isIn=$(echo $?)
+  if [[ ${isIn} -eq 1 ]]; then
     external_ignore[count]=${module_name}
     let count++
   fi
 }
 
-function executeSummary(){
-  if [[ ${enable_summary} -eq 1 ]] && [[ ${#error_module[@]} -gt 0 ]];then
+function push_only_execute_module() {
+  local module_name=$1
+  shift
+  module_name=${module_name%/*}
+  isInArray ${module_name} ${only_execute_modules}
+  isIn=$(echo $?)
+  if [[ ${isIn} -eq 1 ]]; then
+    only_execute_modules[count]=${module_name}
+    let count++
+  fi
+}
+
+function executeSummary() {
+  if [[ ${enable_summary} -eq 1 ]] && [[ ${#error_module[@]} -gt 0 ]]; then
     echo "--------------summary-----------------"
-    for (( i = 0; i < ${#error_module[@]}; i++ )); do
+    for ((i = 0; i < ${#error_module[@]}; i++)); do
       log_error ${error_module[i]}
     done
   fi
 }
 
 function command_check() {
-  $(command -v $1 > /dev/null 2>&1)
+  $(command -v $1 >/dev/null 2>&1)
   canExecute=$(echo $?)
-  if [[ ! ${canExecute} -eq 0 ]];then
+  if [[ ! ${canExecute} -eq 0 ]]; then
     log_error "[$1] is not a command,[$*] can not execute"
     exit 1
   fi
 }
 
+function doExecuteCmd() {
+  local module_name=$1
+  shift
+  cd ${module_name}
+  echo ""
+  log_success "change directory [${module_name}]"
+  moduleExecuteCmd ${module_name} $*
+  returnValue=$(echo $?)
+  if [[ ${enable_summary} -eq 1 ]] && [[ ! ${returnValue} -eq 0 ]]; then
+    error_module+=("module [${file}] execute [$*] error")
+  fi
+  echo ""
+  cd ${rootPath}
+}
+
+function only_execute(){
+  if [[ ${#only_execute_modules[@]} -gt 0 ]];
+    then
+      for ((i = 0;i < ${#only_execute_modules[@]}; i++));do
+        doExecuteCmd ${only_execute_modules[i]} $*
+      done
+    # only execute then exit
+    exit 0
+  fi
+}
+
 function executeCmd() {
   cd ${rootPath}
-  if [[ ${enable_command_check} -eq 1 ]] ;then
+  if [[ ${enable_command_check} -eq 1 ]]; then
     command_check $*
   fi
+  only_execute $*
   for file in $(ls); do
     isInArray ${file} ${_projects_ignore[@]}
     local isIgnore=$(echo $?)
     if [[ ${isIgnore} -eq 1 ]] && [[ -d ${file} ]]; then
       isInArray ${file} ${external_ignore[@]}
       local ext_ignore=$(echo $?)
-      if [[ ${ext_ignore} -eq 1 ]];then
-        cd ${file}
-        echo ""
-        log_success "change directory [${file}]"
+      if [[ ${ext_ignore} -eq 1 ]]; then
         doExecuteCmd ${file} $*
-        returnValue=$(echo $?)
-        if [[ ${enable_summary} -eq 1 ]] && [[ ! ${returnValue} -eq 0 ]];then
-            error_module+=("module [${file}] execute [$*] error")
-        fi
-        echo ""
-        cd ${rootPath}
       else
         log_success "module ${file} is ignored"
       fi
@@ -111,19 +142,23 @@ function executeCmd() {
 }
 
 # parse command options
-while getopts 'e:sc' OPT;do
+while getopts 'e:o:sc' OPT; do
   case ${OPT} in
-    e)
-      push_external_ignore_module ${OPTARG}
-      ;;
-    s)
-      # disable_summary
-      enable_summary=0
-      ;;
-    c)
-      # disable command check
-      enable_command_check=0
-      ;;
+  e)
+    push_external_ignore_module ${OPTARG}
+    ;;
+  o)
+    # only execute specific modules
+    push_only_execute_module ${OPTARG}
+    ;;
+  s)
+    # disable_summary
+    enable_summary=0
+    ;;
+  c)
+    # disable command check
+    enable_command_check=0
+    ;;
   esac
 done
 
